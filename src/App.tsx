c69d8c23635a290a9a59617b50ecaacc0af34d2c
unknown <asad.hnk@gmail.com>
2023-01-26 15:14:30 +0600

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+
+//Pages
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <>
-      <Sidebar />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+const LoginPage: React.FC = () => {
+  return (
+    <>
+      <h1>Login Page</h1>
+    </>
+  );
+};
+export default LoginPage;
